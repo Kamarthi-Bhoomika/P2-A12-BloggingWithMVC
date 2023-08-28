fc5c033d28b734c547d5d76edc0d6361198a29e5
Kamarthi-Bhoomika <kbhoomika05@gmail.com>
2023-08-28 15:01:24 +0530

    Blogging Platform with MVC

---
+++ Models/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebBloggingApp.Models
+{
+    [Table("Post")]
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string Author { get; set; }
+
+    }
+}
